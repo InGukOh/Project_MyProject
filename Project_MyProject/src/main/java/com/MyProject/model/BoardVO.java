c759d803dc179335fb542f364ff30e857a628cd3
InGukOh <dhdlsrnr1231@naver.com>
2022-02-02 23:37:50 +0900

    Update_BoardMApper/BoardVO/Test

--- Project_MyProject/src/main/java/com/MyProject/model/BoardVO.java
+++ Project_MyProject/src/main/java/com/MyProject/model/BoardVO.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class BoardVO {
 
     /* 게시판 번호 */
-    private int bno;
+    private int num;
     
     /* 게시판 제목 */
     private String title;
@@ -21,5 +21,60 @@ public class BoardVO {
     
     /* 수정 날짜 */
     private Date updateDate;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardVO [num=" + num + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
+				+ regdate + ", updateDate=" + updateDate + "]";
+	}
+    
     
 }
